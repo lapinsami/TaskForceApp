4bc20e0cedc9c5686f1a4827e71aaaa3e4bbe101
Sami Lappalainen <lappalainen.sami@outlook.com>
2025-02-17 16:45:37 +0100

    php for reading and writing json file

--- index.php
+++ index.php
@@ -1,3 +1,49 @@
+<?php
+$filename = "data.json";
+$data = [];
+
+if (file_exists($filename)) {
+    $content = trim(file_get_contents($filename));
+
+    if (!empty($content)) {
+        $data = json_decode($content, true);
+
+        if (!is_array($data)) {
+            $data = [];
+        }
+    }
+}
+
+if (count($data) < 1) {
+    $firstTask = [
+        "task" => "Finish html",
+        "done" => true
+    ];
+
+    $secondTask = [
+        "task" => "Add php",
+        "done" => false
+    ];
+
+    $thirdTask = [
+        "task" => "Submit",
+        "done" => false
+    ];
+
+    $fourthTask = [
+        "task" => "Play SoT",
+        "done" => false
+    ];
+
+    $data[][0] = $firstTask;
+    $data[][1] = $secondTask;
+    $data[][2] = $thirdTask;
+    $data[][3] = $fourthTask;
+}
+
+file_put_contents($filename, json_encode($data, JSON_PRETTY_PRINT));
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,7 +55,7 @@
     <div class="page-container">
         <div class="list-container">
             <div class="form-container">
-                <form class="add-form" action="/add.php">
+                <form class="add-form" action="/index.php?add=todo-item">
                     <label for="todo-item">Add an item</label>
                     <input type="text" id="todo-item" name="todo-item" value="">
                     <input type="submit" value="Add">
